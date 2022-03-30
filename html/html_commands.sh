64b7ea71c09eb607179b59546cc7e709f5d54f74
Braden Rich <bradenwrich@gmail.com>
2022-03-30 13:27:29 -0400

    html commands

--- html/html_commands.sh
+++ html/html_commands.sh
@@ -1,1 +1,6 @@
 mkdir -p repos/sample_website
+# Below are the links to Github pages for this Tutorial that work.
+# https://github.com/Braden-077/LearnEnough
+# https://braden-077.github.io/LearnEnough/README.md
+cd repos/sample_website
+open index.html 
