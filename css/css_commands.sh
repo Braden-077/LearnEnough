1eae53aa24e0e388a2d3fadacc2708e541664a92
Braden Rich <bradenwrich@gmail.com>
2022-04-06 17:40:10 -0400

    CSS chapter 3.6-3.8 completion

--- css/css_commands.sh
+++ css/css_commands.sh
@@ -1,4 +1,16 @@
 touch index.html
 touch css_commands.sh
 open index.html
-# all git commands were skippped due to git tutorial having been previously completed.
+# commands related to git init were skippped due to git tutorial having been previously completed.
+git add .
+git commit -m 'chapter 1 of CSS'
+git add .
+git commit -m 'Chapter 1 completion'
+git add .
+git commit -m 'Chapter 2.1-2.2 completion'
+git add .
+git commit -m 'Complete chapter 2 of CSS'
+git add .
+git commit -m 'Chapter 3.1-3.5 completion'
+git add .
+git commit -m ''
