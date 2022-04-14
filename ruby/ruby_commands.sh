e90f921f4adbf0fa54ee87fa32039c625b2d4122
Braden Rich <bradenwrich@gmail.com>
2022-04-14 15:58:25 -0400

    Gemfile and Ruby initialize

---
+++ ruby/ruby_commands.sh
@@ -0,0 +1,18 @@
+irb 
+puts 'Hello, world!'
+print 'Hello, world!'
+quit
+touch ruby_commands.sh
+touch hello.rb
+ruby hello.rb  # Without second given string
+ruby hello.rb # With second given string
+touch hello 
+chmod +x hello
+./hello
+ruby hello
+gem install sinatra
+bundle install 
+ruby hello_app.rb 
+git add . 
+git commit -m 'Gemfile and ruby initialize'
+git push 
